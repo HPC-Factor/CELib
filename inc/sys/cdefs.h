dfecc831ebd420f8f6f6c73e18bc54ebb4b92ee5
Tsubasa Kato <tsubasa@superai.online>
2022-09-21 07:53:47 +0900

    First commit

---
+++ inc/sys/cdefs.h
@@ -0,0 +1,20 @@
+/* cdefs.h - BSD defs */
+
+#ifndef SYS_CDEFS_H
+#define SYS_CDEFS_H
+
+#define IEEEFP 1 /* IEEE Floating point */
+
+#define __P(X) X
+
+#define __BEGIN_DECLS
+#define __END_DECLS
+
+#define __dead2
+
+#define __const const
+#define __aconst const
+
+#define _BSD_VA_LIST_ ...
+#endif
+
